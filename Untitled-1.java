5d5b5d8af609a67e09f2631b5e55ebc6d1a8aa59
Kousthub Ganugapati <55417572+188738@users.noreply.github.com>
2023-09-16 17:32:02 -0500

    Update Untitled-1.java

---
+++ Untitled-1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Matrix
+{
+    int numRows;
+    int numCol;
+    int[][] matrix;
+    public Matrix(int numRow, int numCol){
+        
+        this.numRows = numRows;
+        this.numCol = numCol;
+        
+    }
+    public void generateMatrix(){
+        
+    }
+    
+    public int getRows(){
+        return numRows;
+    }
+    
+    public int getCol(){
+        return numCol;
+    }
+    
+    
+}
