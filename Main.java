fcbc08338e1986a1f3012dbed9f7a2e522f77f10
Kousthub Ganugapati <55417572+188738@users.noreply.github.com>
2023-09-16 22:42:12 -0500

    Update Main.java Add, Subtract, and Multiply

--- Main.java
+++ Main.java
@@ -1,2 +1,130 @@
-public class Main{
+
+import java.util.*;
+public class Main
+{
+    
+
+    public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Number of Matrices 1 or 2?");
+        int numM = console.nextInt();
+        if(numM == 1){
+            //First Matrice Data 
+            System.out.println("First matrice number of rows");
+            int rows = console.nextInt();
+            System.out.println("First matrice number of col");
+            int col = console.nextInt();
+            //Initialize Matrix
+            Matrix m1 = new Matrix(rows,col);
+            
+            //Prompt Operations
+            String[] operations = {"RREF", "Null Space", "Column Space", "Rank"};
+            System.out.println("What operation do you want to apply?");
+            String operation = console.nextLine();
+            if(operation.equals(operations[0])){
+                
+                
+            }
+            else if(operation.equals(operations[1])){
+                
+            }
+            else if(operation.equals(operations[2])){
+                
+            }
+            else if(operation.equals(operations[3])){
+                System.out.println("Rank is " + m1.getRows());
+                
+            }
+        }
+        else{
+            //First Matrice Data 
+            System.out.println("First matrice number of rows");
+            int rows = console.nextInt();
+            System.out.println("First matrice number of col");
+            int col = console.nextInt();
+            //Initialize Matrix
+            Matrix m1 = new Matrix(rows,col);
+            //Second Matrice Data
+            System.out.println("Second matrix number of rows");
+            int sRows = console.nextInt();
+            System.out.println("Second matrix number of col");
+            int sCol = console.nextInt();
+            Matrix m2 = new Matrix(sRows,sCol);
+            String[] operations = {"Addition", "Subtraction", "Multiplication"};
+            System.out.println("What operation do you want to apply?");
+            String operation = console.nextLine();
+            if(operation.equals(operations[0])){
+                
+                
+            }
+            else if(operation.equals(operations[1])){
+                
+            }
+            else if(operation.equals(operations[2])){
+                
+            }
+        }
+        
+        
+        
+    }
+    public static void addMatrice(Matrix m1, Matrix m2){
+        if(m1.getRows() != m2.getRows() || m1.getCol() != m2.getCol()){
+            System.out.println("Not valid");
+        }
+        else{
+             for(int i = 0;i < m1.getRows();i++){
+            for(int j = 0;j < m1.getCol();j++){                
+              System.out.print(m1.getVal(i,j) + m2.getVal(i,j) + " ");
+              
+            }
+            System.out.println();
+        }
+        
+        }
+    }
+    public static void subMatrice(Matrix m1, Matrix m2){
+        if(m1.getRows() != m2.getRows() || m1.getCol() != m2.getCol()){
+            System.out.println("Not valid");
+        }
+        else{
+             for(int i = 0;i < m1.getRows();i++){
+                for(int j = 0;j < m1.getCol();j++){                
+                  System.out.print(m1.getVal(i,j) - m2.getVal(i,j) + " ");
+              
+                }
+                System.out.println();
+            }
+        
+        }
+        
+    }
+    public static void multMatrice(Matrix m1, Matrix m2){
+        if(m1.getRows() != m2.getCol() || m1.getCol() != m2.getRows()){
+            System.out.println("Not valid");
+        }
+        else{
+             int[][] result = new int[m1.getRows()][m2.getCol()];
+
+            for (int i = 0; i < m1.getRows(); i++) {
+            for (int j = 0; j < m2.getCol(); j++) {
+                    int sum = 0;
+                for (int k = 0; k < m1.getCol(); k++) {
+                    sum += m1.getVal(i,k) * m1.getVal(k,j);
+                }
+                result[i][j] = sum;
+            }
+            }
+            for (int[] row : result) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+            }
+        
+        }
+        
+    }
+    
 }
